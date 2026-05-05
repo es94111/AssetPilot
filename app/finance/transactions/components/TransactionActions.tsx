3df6e7aeb8f7a89f144217a884c13849749cd29b
es94111 <45334638+es94111@users.noreply.github.com>
2026-05-05 22:34:14 +0800

    feat: 修復交易記錄編輯視窗交互問題，優化事件處理機制

--- app/finance/transactions/components/TransactionActions.tsx
+++ app/finance/transactions/components/TransactionActions.tsx
@@ -8,13 +8,31 @@ export function TransactionActions({ id }: { id: string }) {
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger className="text-blue-500">編輯</DialogTrigger>
-      <DialogContent>
+      {/* 阻止觸發器事件冒泡到 TableRow */}
+      <DialogTrigger 
+        className="text-blue-500 hover:text-blue-700 cursor-pointer"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        編輯
+      </DialogTrigger>
+      
+      {/* 點擊內容內部時防止冒泡並阻止預設行為 */}
+      <DialogContent onClick={(e) => e.stopPropagation()}>
         <DialogHeader>
           <DialogTitle>編輯交易 {id}</DialogTitle>
         </DialogHeader>
-        {/* TODO: Add Edit form */}
-        <button onClick={() => setOpen(false)}>儲存</button>
+        
+        <div className="p-4">
+          {/* TODO: Add Edit form */}
+          <button 
+            className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+            onClick={() => setOpen(false)}
+          >
+            儲存
+          </button>
+        </div>
       </DialogContent>
     </Dialog>
   );
